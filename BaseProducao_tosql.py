74a48676e594d0fa243b6c64ef21029e0cb3eb22
leandroko <leandro.chko@gmail.com>
2020-10-20 00:25:53 -0300

    a

---
+++ BaseProducao_tosql.py
@@ -0,0 +1,31 @@
+# Bibliotecas usadas no projeto
+import glob
+import pyodbc
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+
+# Conexão com o Banco de Dados
+server = 'localhost'
+database = 'LIUDB'
+sqleng = create_engine('mssql+pyodbc://localhost/LIUDB?driver=SQL Server?Trusted_Connection=yes')
+
+# get data file names
+path =r'C:\Users\Liu\Desktop\Dash'
+filenames = glob.glob(path + "\BaseP*.csv")
+
+
+dfs = []
+for filename in filenames:
+    dfs.append(pd.read_csv(filename, sep=';'))
+
+# Concatenate all data into one DataFrame
+big_frame = pd.concat(dfs, ignore_index=True)
+
+big_frame.to_sql(name='BASEPRODUCAO', con=sqleng, schema='LEANDRO', if_exists='replace' , index=False)
+
+
+
+
+
+
